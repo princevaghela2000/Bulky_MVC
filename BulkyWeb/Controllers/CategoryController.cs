56ab1b8f49260cd8b7de5e912491f24969b32fe4
princevaghela2000 <100965988+princevaghela2000@users.noreply.github.com>
2024-02-28 00:17:38 +0530

    db connection category index page

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _dbContext.Category.OrderBy(o => o.DisplayOrder).ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult CreateCategory() 
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyWeb.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; } = string.Empty;
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
